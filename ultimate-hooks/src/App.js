4ff586ef2a36ca4ba25461ebef94209158bd95c4
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-05-16 19:58:52 +0300

    useResource hook valmis

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -17,11 +17,16 @@ const useField = (type) => {
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
-
-  // ...
-
-  const create = (resource) => {
-    // ...
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
+    fetchData()
+  })
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    return response.data
   }
 
   const service = {
